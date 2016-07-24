2c94bb9bff6086e51d2992a6175c2f176a417b1f
Jihoon Oh <jihoonoh123@gmail.com>
2016-07-24 19:40:12 -0400

    finished skeleton of about page, added yt embed plugin

--- wp-content/themes/tm-beans-child/page-5.php
+++
@@ -1,5 +0,0 @@
-<?php get_header(); ?>
-
-<h1> b </h1>
-
-<?php get_footer(); ?>
---
+++ wp-content/themes/tm-beans-child/page-about.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+<?php 
+	$imgRoot = get_theme_root() . "/tm-beans-child/imgs/Wesley.jpg";
+?>
+<div class="bio" id="wesley">
+	<h1>Wesley Han</h1>
+	<img src='/wp-content/themes/tm-beans-child/imgs/Wesley.jpg'>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa minus nihil ullam aliquid pariatur commodi, porro eveniet nemo, eius officiis inventore. Natus atque rem cum! Omnis, dolore esse qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quaerat mollitia ut pariatur fuga, cumque excepturi delectus expedita magnam libero. Quibusdam delectus doloremque vitae expedita magnam reprehenderit, modi minus maiores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde, quae in rem voluptate voluptas incidunt rerum expedita molestiae repellat. Omnis perferendis officiis labore quis voluptate. Alias facere numquam error!
+	</p>
+</div>
+
+<div class="bio" id="sam">
+	<h1>Sam Jin</h1>
+	<img src='/wp-content/themes/tm-beans-child/imgs/Sam.jpg'>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa minus nihil ullam aliquid pariatur commodi, porro eveniet nemo, eius officiis inventore. Natus atque rem cum! Omnis, dolore esse qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quaerat mollitia ut pariatur fuga, cumque excepturi delectus expedita magnam libero. Quibusdam delectus doloremque vitae expedita magnam reprehenderit, modi minus maiores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde, quae in rem voluptate voluptas incidunt rerum expedita molestiae repellat. Omnis perferendis officiis labore quis voluptate. Alias facere numquam error!
+	</p>
+</div>
+
+<div class="bio" id="alex">
+	<h1>Alex "AP" Park</h1>
+	<img src='/wp-content/themes/tm-beans-child/imgs/Alex.jpg'>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa minus nihil ullam aliquid pariatur commodi, porro eveniet nemo, eius officiis inventore. Natus atque rem cum! Omnis, dolore esse qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quaerat mollitia ut pariatur fuga, cumque excepturi delectus expedita magnam libero. Quibusdam delectus doloremque vitae expedita magnam reprehenderit, modi minus maiores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde, quae in rem voluptate voluptas incidunt rerum expedita molestiae repellat. Omnis perferendis officiis labore quis voluptate. Alias facere numquam error!
+	</p>
+</div>
+
+<div class="bio" id="phill">
+	<h1>Phill Kim</h1>
+	<img src='/wp-content/themes/tm-beans-child/imgs/Phill.jpg'>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa minus nihil ullam aliquid pariatur commodi, porro eveniet nemo, eius officiis inventore. Natus atque rem cum! Omnis, dolore esse qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quaerat mollitia ut pariatur fuga, cumque excepturi delectus expedita magnam libero. Quibusdam delectus doloremque vitae expedita magnam reprehenderit, modi minus maiores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde, quae in rem voluptate voluptas incidunt rerum expedita molestiae repellat. Omnis perferendis officiis labore quis voluptate. Alias facere numquam error!
+	</p>
+</div>
+
+<div class="bio" id="jihoon">
+	<h1>Jihoon Oh</h1>
+	<img src='/wp-content/themes/tm-beans-child/imgs/Me.jpg'>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque ipsa minus nihil ullam aliquid pariatur commodi, porro eveniet nemo, eius officiis inventore. Natus atque rem cum! Omnis, dolore esse qui. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quaerat mollitia ut pariatur fuga, cumque excepturi delectus expedita magnam libero. Quibusdam delectus doloremque vitae expedita magnam reprehenderit, modi minus maiores! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor unde, quae in rem voluptate voluptas incidunt rerum expedita molestiae repellat. Omnis perferendis officiis labore quis voluptate. Alias facere numquam error!
+	</p>
+</div>
+
+<?php get_footer(); ?>
